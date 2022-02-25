bdb28cbbfeaefe4a3a2600ba63894b176a89c727
Eric Engelhart <bob80333@gmail.com>
2022-02-25 16:55:13 -0500

    xsmall was 80% token embeddings, shifted sizes

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     # model sizes based on "The Depth-to-Width interplay in Self-Attention", https://arxiv.org/abs/2006.12467
     # specifically Figure 3(c) and Appendix D Table 4(a)
     parser.add_argument("--model-size",
-                        choices=["xsmall", "small", "medium", "large", "xlarge"], type=str,
+                        choices=["xsmall", "small", "medium", "large"], type=str,
                         default="xsmall", help="model sizes")
 
     parser.add_argument("--max-context-length", type=int, default=512)
@@ -50,23 +50,19 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.model_size == "xsmall":  # 1.78M non embedding parameters
-        n_layers = 6
-        width = 192
-        n_heads = 3
-    elif args.model_size == "small":  # 6.3M non embedding parameters
+    if args.model_size == "xsmall":  # 6.3M non embedding parameters
         n_layers = 12
         width = 256
         n_heads = 4
-    elif args.model_size == "medium":  # 21.3M non embedding parameters
+    elif args.model_size == "small":  # 21.3M non embedding parameters
         n_layers = 18
         width = 384
         n_heads = 6
-    elif args.model_size == "large":  # 50.4M non embedding parameters
+    elif args.model_size == "medium":  # 50.4M non embedding parameters
         n_layers = 24
         width = 512
         n_heads = 8
-    elif args.model_size == "xlarge":  # 141.8M non embedding parameters
+    elif args.model_size == "large":  # 141.8M non embedding parameters
         n_layers = 30
         width = 768
         n_heads = 12
