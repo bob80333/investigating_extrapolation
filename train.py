36b8bce7c16d2ba5b087e1705f4bc13609e22714
Eric Engelhart <bob80333@gmail.com>
2022-02-25 10:53:57 -0500

    redo model sizing based on paper

--- train.py
+++ train.py
@@ -11,9 +11,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
+    # model sizes based on "The Depth-to-Width interplay in Self-Attention", https://arxiv.org/abs/2006.12467
+    # specifically Figure 3(c) and Appendix D Table 4(a)
     parser.add_argument("--model-size",
-                        choices=["2.5M", "4.3M", "10.4M", "21.2M", "63.4M", "143.3M", "273.5M", "466.7M"], type=str,
-                        default="4.3M")
+                        choices=["xsmall", "small", "medium", "large", "xlarge"], type=str,
+                        default="xlarge", help="model sizes")
 
     parser.add_argument("--max-context-length", type=int, default=512)
     parser.add_argument("--train-context-length", type=int, default=128)
@@ -24,38 +26,26 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.model_size == "2.5M":
-        n_layers = 3
+    if args.model_size == "xsmall":  # 1.78M non embedding parameters
+        n_layers = 6
         width = 192
         n_heads = 3
-    elif args.model_size == "4.3M":
-        n_layers = 4
+    elif args.model_size == "small":  # 6.3M non embedding parameters
+        n_layers = 12
         width = 256
         n_heads = 4
-    elif args.model_size == "10.4M":
-        n_layers = 6
+    elif args.model_size == "medium":  # 21.3M non embedding parameters
+        n_layers = 18
         width = 384
         n_heads = 6
-    elif args.model_size == "21.2M":
-        n_layers = 8
+    elif args.model_size == "large":  # 50.4M non embedding parameters
+        n_layers = 24
         width = 512
         n_heads = 8
-    elif args.model_size == "63.4M":
-        n_layers = 12
+    elif args.model_size == "xlarge":  # 141.8M non embedding parameters
+        n_layers = 30
         width = 768
         n_heads = 12
-    elif args.model_size == "143.3M":
-        n_layers = 16
-        width = 1024
-        n_heads = 16
-    elif args.model_size == "273.5M":
-        n_layers = 20
-        width = 1280
-        n_heads = 20
-    elif args.model_size == "466.7M":
-        n_layers = 24
-        width = 1536
-        n_heads = 24
     else:
         raise Exception("invalid model choice")
 
@@ -69,7 +59,3 @@ if __name__ == "__main__":
     print("Initialized transformer model with", (total_params // 10_000) / 100, "M parameters")
 
     optimizer = optim.Adam(model.parameters(), lr=6e-4, betas=(0.9, 0.95), eps=1e-8, weight_decay=0.1)
-
-    
-
-
