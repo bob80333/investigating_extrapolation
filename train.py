be05914e641cd14ea66a21992bcb4ca8e41b100b
Eric Engelhart <bob80333@gmail.com>
2022-02-25 22:26:09 -0500

    redo models sizes based on gpt-3/gpt-2

--- train.py
+++ train.py
@@ -30,10 +30,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    # model sizes based on "The Depth-to-Width interplay in Self-Attention", https://arxiv.org/abs/2006.12467
-    # specifically Figure 3(c) and Appendix D Table 4(a)
     parser.add_argument("--model-size",
-                        choices=["xsmall", "small", "medium", "large"], type=str,
+                        choices=["xsmall", "small", "medium", "large", "xlarge"], type=str,
                         default="xsmall", help="model sizes")
 
     parser.add_argument("--max-context-length", type=int, default=512)
@@ -50,22 +48,26 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.model_size == "xsmall":  # 6.3M non embedding parameters
-        n_layers = 12
-        width = 256
-        n_heads = 4
-    elif args.model_size == "small":  # 21.3M non embedding parameters
-        n_layers = 18
-        width = 384
-        n_heads = 6
-    elif args.model_size == "medium":  # 50.4M non embedding parameters
-        n_layers = 24
+    if args.model_size == "xsmall":  # 16.8M non embedding parameters
+        n_layers = 8
         width = 512
         n_heads = 8
-    elif args.model_size == "large":  # 141.8M non embedding parameters
-        n_layers = 30
+    elif args.model_size == "small":  # 56.7M non embedding parameters
+        n_layers = 12
         width = 768
         n_heads = 12
+    elif args.model_size == "medium":  # 134.3M non embedding parameters
+        n_layers = 16
+        width = 1024
+        n_heads = 16
+    elif args.model_size == "large":  # 262.4M non embedding parameters
+        n_layers = 20
+        width = 1280
+        n_heads = 20
+    elif args.model_size == "xlarge": # 453.3M non embedding parameters
+        n_layers = 24
+        width = 1536
+        n_heads = 24
     else:
         raise Exception("invalid model choice")
 
@@ -130,7 +132,7 @@ if __name__ == "__main__":
 
     valid_datasets = [
         TextDataset(list(Path("ao3_small_dataset/valid").rglob("*.tok")), "byte_tokenized_16k.json", test_length,
-                    stride=args.train_context_length//2, pretokenized=True) for test_length in args.test_context_lengths]
+                    stride=args.test_length, pretokenized=True) for test_length in args.test_context_lengths]
 
     with torch.inference_mode():
         for valid_dataset, test_length in zip(valid_datasets, args.test_context_lengths):
