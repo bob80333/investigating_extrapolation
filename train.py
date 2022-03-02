aab80c3ff1232d11014ad109ad976dc02c9372ea
Eric Engelhart <bob80333@gmail.com>
2022-03-02 12:17:52 -0500

    Training/testing of relative pos embeddings

--- train.py
+++ train.py
@@ -43,8 +43,8 @@ if __name__ == "__main__":
 
     parser.add_argument("--absolute-position-embedding", choices=["sinusoidal", "scaled_sinusoidal", "learned", "none"],
                         type=str, default="none")
+    parser.add_argument("--relative-position-embedding", choices=["linear_cpb", "log_cpb", "alibi", "rotary", "none"])
 
-    # TODO: add in new relative position embedding options
     parser.add_argument("--num-train-steps", type=int, default=10_000)
     parser.add_argument("--batch-size", type=int, default=128)
     parser.add_argument("--clipping", type=float, default=1.0)
@@ -76,9 +76,19 @@ if __name__ == "__main__":
     else:
         raise Exception("invalid model choice")
 
-    # TODO: correctly set model length for training with relative pos embeds
-    model = Encoder(8192, width, n_layers, n_heads, width * 2, 0.1, torch.device("cuda"), args.max_context_length,
-                    args.absolute_position_embedding).cuda()
+    # if using relative pos embeddings
+    # and the absolute embeddings are the kind that can be length adjusted after training (none/sinusoidal)
+    # then set model max context length to training length
+    if args.relative_position_embedding != "none" and args.absolute_position_embedding in ["none", "sinusoidal", "scaled_sinusoidal"]:
+        model = Encoder(8192, width, n_layers, n_heads, width * 2, 0.1, torch.device("cuda"), args.train_context_length,
+                        args.absolute_position_embedding, args.relative_position_embedding).cuda()
+    # otherwise set max context length to max context length if no relative pos embeddings
+    elif args.relative_position_embedding == "none":
+        model = Encoder(8192, width, n_layers, n_heads, width * 2, 0.1, torch.device("cuda"), args.max_context_length,
+                        args.absolute_position_embedding, args.relative_position_embedding).cuda()
+    # learned pos embeddings + relative pos embeddings don't mix
+    else:
+        raise ValueError("Cannot have both learned absolute embeddings and relative embeddings (for now)")
 
     total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     non_embedding_params = sum(
@@ -147,7 +157,6 @@ if __name__ == "__main__":
 
     avg_losses = []
     median_losses = []
-    # TODO: correctly modify model seq len for relative pos embeds for testing
     with torch.inference_mode():
         for valid_dataset, test_length in zip(valid_datasets, args.test_context_lengths):
             losses = np.array(np.zeros((valid_dataset.length, test_length-1)))
@@ -155,6 +164,11 @@ if __name__ == "__main__":
             current_idx = 0
 
             valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+
+            if args.relative_position_embedding != "none" and args.absolute_position_embedding in ["none", "sinusoidal",
+                                                                                                   "scaled_sinusoidal"]:
+                model.update_sizes(test_length)
+
             for batch in tqdm(valid_dataloader):
                 batch = batch.cuda()
 
